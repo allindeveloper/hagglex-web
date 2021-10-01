cee016930847f755471cd98e2d908fdcf6203527
Precious Uchendu <uchendu.precious@thegiggroupng.com>
2021-10-01 02:02:52 +0100

    styling auth carousel

--- src/components/shared/AuthCarousel.tsx
+++ src/components/shared/AuthCarousel.tsx
@@ -1,81 +1,62 @@
-import { ReactChild, useState } from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import authCarouselStyles from '../../styles/authCarouselStyles';
-
-
+import { ReactChild, useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import authCarouselStyles from "../../styles/authCarouselStyles";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 interface IAuthCarousel {
-    children?:ReactChild[]
+  children?: ReactChild[];
 }
-const AuthCarousel = ({children}:IAuthCarousel)=>{
+const AuthCarousel = ({ children }: IAuthCarousel) => {
+  const [currentSlide, setcurrentSlide] = useState(0);
+  const next = () => {
+    setcurrentSlide(currentSlide + 1);
+  };
+  const classes = authCarouselStyles();
 
-        const [currentSlide, setcurrentSlide] = useState(0)
-        const next = () => {
-            setcurrentSlide(currentSlide+1)
-         
-        };
-        const classes = authCarouselStyles()
+  const prev = () => {
+    setcurrentSlide(currentSlide - 1);
+  };
 
-       const prev = () => {
-            setcurrentSlide(currentSlide-1)
-           
-        };
-
-        const updateCurrentSlide = (index:number) => {
-
-            if (currentSlide !== index) {
-                setcurrentSlide(index)
-            }
-        };
-        const renderIndicators = (
-            clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
-            isSelected: boolean,
-            index: number,
-            label: string
-        )=> {
-            
-            return (
-                <div className={classes.indicatorsRoot} style={{visibility:isSelected?'visible':'hidden'}}>
-                    <div>
-                    left
-                    </div>
-                    <div>
-                        igoioij
-                    </div>
-                    <div>
-                        right
-                    </div>
-                </div>
-            )
-
-        }
-
-            const buttonStyle = { fontSize: 20, padding: '5px 20px', margin: '5px 0px' };
-            const containerStyle = { margin: '5px 0 20px' };
-            return (
-                <div>
-                    {/* <div style={containerStyle}>
-                        <button onClick={prev} style={buttonStyle}>
-                            Prev
-                        </button>
-                        <button onClick={next} style={buttonStyle}>
-                            Next
-                        </button>
-                      
-                    </div> */}
-                    <Carousel
-                        autoPlay={false}
-                        showArrows={false}
-                        showIndicators={true}
-                        showStatus={false}
-                        showThumbs={false}
-                        renderIndicator={renderIndicators}
-                        selectedItem={currentSlide}
-                        onChange={updateCurrentSlide}
-                    >
-                        {children}
-                    </Carousel>
-                </div>
-            );
+  const updateCurrentSlide = (index: number) => {
+    if (currentSlide !== index) {
+      setcurrentSlide(index);
+    }
+  };
+  const renderIndicators = (
+    clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
+    isSelected: boolean,
+    index: number,
+    label: string
+  ) => {
+    return (
+      <>
+        {isSelected && (
+          <div className={classes.indicatorsRoot}>
+            <div className="me-5 cursor-pointer" onClick={prev}><ArrowBackIosIcon htmlColor={currentSlide === 0 ?  'blue':'white'} /></div>
+            <div>igoioij</div>
+            <div className="ms-5 cursor-pointer" onClick={next}><ArrowForwardIosIcon htmlColor={currentSlide === 2? 'blue':'white'}/></div>
+          </div>
+        )}
+      </>
+    );
+  };
+  return (
+    <div>
+     <Carousel
+        autoPlay={false}
+        showArrows={false}
+        showIndicators={true}
+        showStatus={false}
+        showThumbs={false}
+        renderIndicator={renderIndicators}
+        selectedItem={currentSlide}
+        useKeyboardArrows
+        onChange={updateCurrentSlide}
+      >
+        {children}
+      </Carousel>
+    </div>
+  );
 };
 
-export default AuthCarousel
+export default AuthCarousel;
--- src/components/shared/OnboardingRightContainer.tsx
+++ src/components/shared/OnboardingRightContainer.tsx
@@ -5,20 +5,21 @@ import tradeglobally from "../../assets/svg/tradeglobally.svg";
 export interface OnboardingRightContainer {}
 const OnboardingRightContainer: React.FC<OnboardingRightContainer> = () => {
   const classes = LoginStyles();
+
   return (
     <div className={classes.logotypeContainer}>
       <AuthCarousel>
-        <div className="m-5">
+        <div className="">
           <img src={tradeglobally} width={"10%"} />
           <h2>Trade Globally</h2>
-          <p className="mb-5">
+          <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
             et ea rebum. Stet clita kasd
           </p>
         </div>
-        <div>
+        <div className="">
           <img src={tradeglobally} width={"10%"} />
           <h2>Trade Securely</h2>
           <p>
@@ -28,7 +29,7 @@ const OnboardingRightContainer: React.FC<OnboardingRightContainer> = () => {
             et ea rebum. Stet clita kasd
           </p>
         </div>
-        <div>
+        <div className="">
           <img src={tradeglobally} width={"10%"} />
           <h2>Trade Smartly</h2>
           <p>
